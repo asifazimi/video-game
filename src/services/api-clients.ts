699cc334017049c38df1e1d032880b781a362664
Mohammad Asif Azimi <asifazimi.me@gmail.com>
2025-02-17 16:39:42 +0100

    fix the bugs

--- src/services/api-clients.ts
+++ src/services/api-clients.ts
@@ -3,6 +3,7 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
-    key: import.meta.env.VITE_RAWG_API_KEY,
+    // key: import.meta.env.VITE_RAWG_API_KEY,
+    key: "ec17b393d84f423aa541cfc7e7b4088c",
   },
 });
