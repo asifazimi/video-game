ae8a2ab6e63eb375ee1c0aac19723dd4abcff478
Mohammad Asif Azimi <asifazimi.me@gmail.com>
2025-02-17 16:23:27 +0100

    securing the app

--- src/services/api-clients.ts
+++ src/services/api-clients.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
-    key: "d2a92d70d1c34123881b223c60e014d0",
+    key: import.meta.env.VITE_RAWG_API_KEY,
   },
 });
