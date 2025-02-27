66bd1ca649f25d3aad50a81b4b069d90ebc58e34
Mohammad Asif Azimi <asifazimi.me@gmail.com>
2025-02-27 13:17:06 +0100

    Making the genres and platforms render statatically

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,4 +1,5 @@
-import useData from "./useData";
+// import useData from "./useData";
+import genres from "../data/genres";
 
 export interface Genre {
   id: number;
@@ -7,6 +8,7 @@ export interface Genre {
   slug: string;
 }
 
-const useGenres = () => useData<Genre>("/genres");
+// const useGenres = () => useData<Genre>("/genres");
+const useGenres = () => ({ data: genres, loading: false, errors: null }); // Here because we want to render the genres statically
 
 export default useGenres;
--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,14 +1,16 @@
-import useData from "./useData";
+import platforms from "../data/platforms";
+// import useData from "./useData";
 
-interface PlatformParent {
-  id: number;
-  name: string;
-  slug: string;
-}
+// interface PlatformParent {
+//   id: number;
+//   name: string;
+//   slug: string;
+// }
 
 // /platforms/lists/parents
 const usePlatforms = () => {
-  return useData<PlatformParent>("/platforms/lists/parents");
+  // return useData<PlatformParent>("/platforms/lists/parents");
+  return { data: platforms, errors: null }; // we want to make the platforms to render statically
 };
 
 export default usePlatforms;
